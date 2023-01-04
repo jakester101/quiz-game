faa145124207b19093dbf1b30269f6fe3d7b7b0d
jakester101 <55941541+jakester101@users.noreply.github.com>
2023-01-03 20:57:16 -0500

    added game over screen by injecting elemnts thru javascript

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,41 @@ timer.textContent = "Time: 5"
 
 
 
-main.textContent = "words words words";
+
 
 //timer function
+
+function gameOver(){
+var allDone = document.getElementById("h1");
+allDone.innerHTML = "All done!";
+document.getElementById("h3").innerHTML = "Your final score is " + secondsLeft;
+document.getElementById("start").innerHTML = " ";
+
+var form = document.createElement('form');
+form.setAttribute("action",'./assets/highscores.html');
+var label = document.createElement('label');
+label.textContent = 'Enter your initials:  ';
+var input = document.createElement('input');
+input.setAttribute('type','text');
+
+var input2 = document.createElement('input');
+input2.setAttribute('type','submit');
+
+form.appendChild(label);
+form.appendChild(input);
+form.appendChild(input2);
+
+
+var bottom = document.getElementById('main1');
+bottom.appendChild(form);
+
+
+}
+
+
+
+
+
 var secondsLeft = 5;
 function setTime() {
     // Sets interval in variable
@@ -25,5 +57,24 @@ function setTime() {
   
     }, 1000);
   }
+
+function gameStart(){
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+var start = document.body.querySelector("#start");  
+start.addEventListener("click", setTime);
+start.addEventListener("click", gameStart);
 
 
